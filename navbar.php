426df559aef0cf695c59bbafd1f0a077c75bf089
FERRARIOloic <ferrarioloic@gmail.com>
2022-05-24 11:36:28 +0200

    création de la page accueil et contact

---
+++ navbar.php
@@ -0,0 +1,8 @@
+
+    <header class="container-fluid">
+        <nav class="navbar bg-light">
+            <a class="navbar-brand" href="index.html">
+                <h1><img src="public/assets/img/icons/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">Mon P'tit Truc En Bois</h1>
+            </a>
+        </nav>
+    </header>
