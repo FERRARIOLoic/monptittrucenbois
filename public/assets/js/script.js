164b871032ef5d21e7b51c7f88512b6668e784ca
FERRARIOloic <ferrarioloic@gmail.com>
2022-05-25 16:38:42 +0200

    ajout de la page de connexion et profil

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -63,3 +63,88 @@ function modalInclude() {
         }
     }
 }
+
+
+/*------------- CONNECT VUE ---------*/
+
+let connectVueInscription = () => {
+    connectVue.innerHTML = `
+    <div id="cartVueModal">
+        <div class="row pt-3">
+            <div class="col-12 col-md-5 px-4 py-2">
+                Identifiant
+            </div>
+            <div class="col-12 col-md-7 px-4 py-2">
+                <input type="text" class="inputText">
+            </div>
+        </div>
+        <div class="row pt-3">
+            <div class="col-12 col-md-5 px-4 py-2">
+                Adresse email
+            </div>
+            <div class="col-12 col-md-7 px-4 py-2">
+                <input type="email" class="inputText">
+            </div>
+        </div>
+        <div class="row pt-3">
+            <div class="col-12 col-md-5 px-4 py-1 pt-2">
+                Mot de passe
+            </div>
+            <div class="col-12 col-md-7 px-4 py-1">
+                <input type="text" class="inputText">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-5 px-4 py-1">
+                Confirmer le Mot de passe
+            </div>
+            <div class="col-12 col-md-7 px-4 py-1">
+                <input type="text" class="inputText">
+            </div>
+        </div>
+    </div>
+    <div id="cartFooterVue" class="border-1 border-top p-3">
+        <div class="row">
+            <div class="col-12 text-end order-1">
+                <button type="submit" class="btn btn-secondary">Créer un compte</button>
+            </div>
+        </div>
+    </div>
+    `;
+}
+let connectVueConnexion = () => {
+    connectVue.innerHTML = `
+    <div id="cartVueModal">
+        <div class="row pt-3">
+            <div class="col-12 col-md-5 px-4 py-2">
+                Identifiant
+            </div>
+            <div class="col-12 col-md-7 px-4 py-2">
+                <input type="text" class="inputText">
+            </div>
+        </div>
+        <div class="row pt-3">
+            <div class="col-12 col-md-5 px-4 py-2">
+                Mot de passe
+            </div>
+            <div class="col-12 col-md-7 px-4 py-2">
+                <input type="text" class="inputText">
+            </div>
+        </div>
+    </div>
+    <div id="cartFooterVue" class="border-1 border-top p-3">
+        <div class="row">
+            <div class="col-12 text-end order-1">
+                <button type="submit" class="btn btn-secondary">Se connecter</button>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+
+
+/*------------- BUTTONS ADD EVENT LISTENER ---------*/
+
+btnInscription.addEventListener('click', connectVueInscription);
+btnConnexion.addEventListener('click', connectVueConnexion);
